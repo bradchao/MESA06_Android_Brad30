87eb86f023ed8262420bc8dad5a42346e32bec8d
Brad Chao <brad@brad.tw>
2016-12-02 13:58:32 +0800

    v2

--- app/src/main/java/com/example/user/brad30/MyService.java
+++ app/src/main/java/com/example/user/brad30/MyService.java
@@ -4,13 +4,35 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyService extends Service {
-    public MyService() {
-    }
+    private Timer timer;
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        timer = new Timer();
+        timer.schedule(new MyTask(), 10*1000);
+    }
+
+    private class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            sendNotice();
+        }
+    }
+
+    private void sendNotice(){
+
+    }
+
 }
