771db725458836b0afda168d2f60b8e8d9864bf0
Brad Chao <brad@brad.tw>
2016-12-02 15:33:28 +0800

    v5

--- app/src/main/java/com/example/user/brad30/MainActivity.java
+++ app/src/main/java/com/example/user/brad30/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.user.brad30;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        welcome = (TextView)findViewById(R.id.welcome);
+
+        welcome.setText(R.string.mesg_welcome);
+
 
         Intent it = new Intent(this, MyService.class);
         startService(it);
